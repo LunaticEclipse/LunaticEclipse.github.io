62e9844310c7ef3317acbc2209ebaf14779e1f7c
LunaticEclipse <110119383+LunaticEclipse@users.noreply.github.com>
2022-09-11 12:41:33 +0800

    adding math functions, adding bullet pattern

---
+++ mathFunctions.js
@@ -0,0 +1,45 @@
+var pi = Math.PI
+
+function sqrt(x){
+	return Math.sqrt(x);
+}
+
+function abs(x){
+	return Math.abs(x);
+}
+
+function sin(x){
+	return Math.sin(x);
+}
+
+function cos(x){
+	return Math.cos(x);
+}
+
+function tan(x){
+	return Math.tan(x);
+}
+
+function asin(x){
+	return Math.asin(x);
+}
+
+function acos(x){
+	return Math.acos(x);
+}
+
+function tan(x){
+	return Math.atan(x);
+}
+
+function csc(x){
+	return 1/sin(x);
+}
+
+function sec(x){
+	return 1/cos(x);
+}
+
+function cot(x){
+	return 1/tan(x);
+}
